bc4082f8f3295b873ff6f42674747779cfdafd38
Scott LaBounty <slabounty@gmail.com>
2014-10-02 20:11:30 -0700

    Add Engine File and Colorpicker Files

--- lib/bootstrap/colorpicker2/rails.rb
+++
@@ -1,9 +0,0 @@
-require "bootstrap/colorpicker2/rails/version"
-
-module Bootstrap
-  module Colorpicker2
-    module Rails
-      # Your code goes here...
-    end
-  end
-end
---
+++ lib/bootstrap/colorpicker2/rails/engine.rb
@@ -0,0 +1,8 @@
+module Bootstrap
+  module Colorpicker2
+    module Rails
+      class Engine < ::Rails::Engine
+      end
+    end
+  end
+end
